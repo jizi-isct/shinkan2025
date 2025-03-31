8344e3fe870ae623a2a5f90c4818b47c5893ea97
pasmophobia <l@leonmatsuistroemsvik.com>
2025-03-31 13:51:42 +0200

    feat: add github link to the footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,22 @@
 <div class="root">
+    <a href="https://github.com/JIZI-Network/shinkan2025">
+        <img src="GitHub_Invertocat_Dark.svg" alt="GitHub Logo" />
+    </a>
     <p>©︎ 2025 JIZI</p>
     <p>contact: tokyotech.shinkan@gmail.com</p>
 </div>
 
 <style>
+    .root {
+        align-items: center;
+        text-align: center;
+    }
     p {
         text-align: center;
         color: #333;
+    }
+
+    img {
+        width: 1.5em;
     }
 </style>
